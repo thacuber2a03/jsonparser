f5f30bb711cad78337e817449a6fe687cafd353d
thacuber2a03 <antonidiaz200731@gmail.com>
2023-08-23 01:45:49 -0400

    load a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, process::exit};
+
+fn main() -> std::io::Result<()> {
+    let argv = std::env::args().collect::<Vec<String>>();
+    if argv.len() <= 1 {
+        println!("usage: {} <filename>", argv[0]);
+        exit(-1);
+    }
+
+    let str = fs::read_to_string(&argv[1]).unwrap_or_else(|e| {
+        println!("couldn't read \"{}\": {e}", argv[1]);
+        exit(-1);
+    });
+
+    println!("{str}");
+
+    Ok(())
 }
